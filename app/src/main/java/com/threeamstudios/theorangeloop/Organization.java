33239f641cb7db4b9bdc7b1ccc14cb4268b46272
Joshua Marasigan <marasigan.josh.villeno@gmail.com>
2016-04-28 20:52:45 -0500

    Fix all the stuff Yoon broke
    Jesus Christ

--- app/src/main/java/com/threeamstudios/theorangeloop/Organization.java
+++ app/src/main/java/com/threeamstudios/theorangeloop/Organization.java
@@ -12,6 +12,18 @@ public class Organization {
     private static Integer orgSize;
     private static ArrayList<Member> orgMembers;
 
+    public Organization(){
+        this.orgName = null;
+        orgSize = 0;
+        orgMembers = null;
+    }
+
+    public Organization(String orgName, Integer orgSize, ArrayList<Member> orgMembers){
+        this.orgName = orgName;
+        this.orgSize = orgSize;
+        this.orgMembers = orgMembers;
+    }
+
     // Additional getters and setters which calls OrganizationController
     public String getOrgName() {
         return orgName;
@@ -21,5 +33,17 @@ public class Organization {
     }
     public ArrayList<Member> getOrgMembers() {
         return orgMembers;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public void setOrgSize(Integer orgSize) {
+        Organization.orgSize = orgSize;
+    }
+
+    public void setOrgMembers(ArrayList<Member> orgMembers) {
+        Organization.orgMembers = orgMembers;
     }
 }

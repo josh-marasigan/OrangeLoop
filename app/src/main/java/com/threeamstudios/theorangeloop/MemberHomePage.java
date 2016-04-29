44ef64849c5ed13682175ae3ca6a7595f4da5b07
Joshua Marasigan <marasigan.josh.villeno@gmail.com>
2016-04-29 10:42:12 -0500

    I am a software God
    Fixed http request issues. Able to get images from imgur.

--- app/src/main/java/com/threeamstudios/theorangeloop/MemberHomePage.java
+++ app/src/main/java/com/threeamstudios/theorangeloop/MemberHomePage.java
@@ -2,12 +2,26 @@ package com.threeamstudios.theorangeloop;
 
 import android.content.Intent;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MemberHomePage extends Activity {
+
+    public static final String URL =
+            "https://i.imgur.com/ZCUZZfw.jpg";
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,13 @@ public class MemberHomePage extends Activity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_member_home_page);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        // Create an object for subclass of AsyncTask
+        GetXMLTask task = new GetXMLTask();
+        // Execute the task
+        task.execute(new String[]{URL});
     }
 
     // Executes with button interaction. Goes to club event page
@@ -27,4 +48,61 @@ public class MemberHomePage extends Activity {
         Intent transition = new Intent(this, EventPage.class);
         startActivity(transition);
     }
+
+    private class GetXMLTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap map = null;
+            for (String url : urls) {
+                map = downloadImage(url);
+            }
+            return map;
+        }
+
+        // Sets the Bitmap returned by doInBackground
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+
+        // Creates Bitmap from InputStream and returns it
+        private Bitmap downloadImage(String url) {
+            Bitmap bitmap = null;
+            InputStream stream = null;
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inSampleSize = 1;
+
+            try {
+                stream = getHttpConnection(url);
+                bitmap = BitmapFactory.
+                        decodeStream(stream, null, bmOptions);
+                stream.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        // Makes HttpURLConnection and returns InputStream
+        private InputStream getHttpConnection(String urlString)
+                throws IOException {
+            InputStream stream = null;
+            java.net.URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+
+            try {
+                HttpURLConnection httpConnection = (HttpURLConnection) connection;
+                httpConnection.setRequestMethod("GET");
+                httpConnection.connect();
+
+                if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    stream = httpConnection.getInputStream();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return stream;
+        }
+    }
+
 }

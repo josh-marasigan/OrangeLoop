158ecaeae73d0bfc853088d6e2118130d4c33a99
Joshua Marasigan <marasigan.josh.villeno@gmail.com>
2016-05-03 01:37:36 -0500

    Implemented sqlite DB interface
    Able to add orgs

--- app/src/main/java/com/threeamstudios/theorangeloop/DatabaseHandler.java
+++ app/src/main/java/com/threeamstudios/theorangeloop/DatabaseHandler.java
@@ -1,12 +1,14 @@
 package com.threeamstudios.theorangeloop;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.content.Context;
 import java.util.ArrayList;
+
+
 
 // Abstract to ensure Singleton design. Only one instantiation is allowed for am SQLite DB.
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -40,11 +42,11 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     // ORGANIZATION Table fields/Columns
     private static final String ORG_ID = "id";
-    private static final String ORG_NAME = "name";
-    private static final String ORG_DESC = "description";
+    protected static final String ORG_NAME = "name";
+    protected static final String ORG_DESC = "desc";
 
     // Retrieve following data from controller
-    private static final String ORG_SIZE = "size";
+    protected static final String ORG_SIZE = "size";
 
     // MEMBER Table fields/Columns
     private static final String MEMBER_ID = "id";
@@ -58,31 +60,33 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         String CREATE_ORG_TABLE =
                 "CREATE TABLE " + TABLE_ORGANIZATIONS
                         + "("
-                        + ORG_ID + " INTEGER PRIMARY KEY, "
-                        + ORG_NAME + " TEXT, "
-                        + ORG_DESC + " TEXT, "
-                        + ORG_SIZE + " INTEGER "
+                        + "id" + " INTEGER PRIMARY KEY, "
+                        + "name" + " TEXT, "
+                        //      + ORG_DESC + " TEXT, "
+                        + "size" + " INTEGER, "
+                        + "desc" + " TEXT"
                         + ")";
-        /* String CREATE_MEMBERS_TABLE =
+  /*      String CREATE_MEMBERS_TABLE =
                 "CREATE TABLE " + TABLE_MEMBERS
                         + "("
                         + MEMBER_ID + " INTEGER PRIMARY KEY, "
                         + MEMBER_NAME + " TEXT "
-                        + ")"; */
-
+                        + ")";
+*/
         // Execute commands
         db.execSQL(CREATE_ORG_TABLE);
-        // db.execSQL(CREATE_MEMBERS_TABLE);
+//        db.execSQL(CREATE_MEMBERS_TABLE);
     }
 
     public boolean insertOrganization(String name, String desc, String size){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("name", name);
-        contentValues.put("desc", desc);
         contentValues.put("size", size);
-        db.insert(TABLE_ORGANIZATIONS, null, contentValues);
-       // db.insert("organizations", null, contentValues);
+        contentValues.put("desc", desc);
+
+        //db.insert(TABLE_ORGANIZATIONS, null, contentValues);
+        db.insert("organizations", null, contentValues);
         return true;
     }
 
@@ -96,8 +100,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("name", name);
-        contentValues.put("desc", desc);
         contentValues.put("size", size);
+        contentValues.put("desc", desc);
         db.update(TABLE_ORGANIZATIONS, contentValues, "id = ? ", new String[] { Integer.toString(id) } );
         return true;
     }
@@ -113,22 +117,30 @@ public class DatabaseHandler extends SQLiteOpenHelper {
                 "id = ? ",
                 new String[] { Integer.toString(id) });
     }
+    public void removeAll(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete("organizations",null,null);
+    }
+
+
+
+
     public ArrayList<String> getAllOrganization(){
         ArrayList<String> array_list = new ArrayList<String>();
 
         SQLiteDatabase db = this.getReadableDatabase();
         //Cursor res =  db.rawQuery( "select name from organizations", null );
         Cursor res = null;
-            String Query ="SELECT * FROM organizations";
+        String Query ="SELECT * FROM organizations";
 
-            res = db.rawQuery(Query,null);
-            if(res != null && res.moveToFirst()){
-                do{
-                    array_list.add(res.getString(res.getColumnIndex(ORG_NAME)));
-                }while(res.moveToNext());
+        res = db.rawQuery(Query,null);
+        if(res != null && res.moveToFirst()){
+            do{
+                array_list.add(res.getString(res.getColumnIndex(ORG_NAME)));
+            }while(res.moveToNext());
 
-            }
-            res.close();
+        }
+        res.close();
 
      /*   res.moveToFirst();
 
